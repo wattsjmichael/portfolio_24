c08b4fb7b80860a2f5019392e44d677149523029
Michael Watts <wattsjmichael@gmail.com>
2024-06-28 14:44:18 -0700

    Updated to use react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // src/App.js
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Timeline from './components/Timeline';
 import Contact from './components/Contact';
+import HomePage from './components/HomePage';
 import './styles.css';
 
 const usePageViews = () => {
-  let location = useLocation();
+  const location = useLocation();
   useEffect(() => {
     if (window.gtag) {
       window.gtag('config', 'G-ZKJE5EH3BJ', {
@@ -24,20 +24,16 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <Helmet>
-          <title>Michael Watts</title>
-          <meta name="description" content="Michael Watts - VR Developer & Project Manager" />
-        </Helmet>
         <Header />
         <div className="scroll-indicator">
           <p>SCROLL</p>
           <div className="line"></div>
         </div>
         <main>
-          <Switch>
-            <Route path="/timeline" component={Timeline} />
-            <Route path="/" exact component={() => <div>Home Page</div>} />
-          </Switch>
+          <Routes>
+            <Route path="/timeline" element={<Timeline />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
         </main>
         <Contact />
       </div>
