dd22e388b51dd0172a21119543ad4f91fa93d31b
Michael Watts <wattsjmichael@gmail.com>
2024-06-28 14:38:14 -0700

    Google analytics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Timeline from './components/Timeline';
 import Contact from './components/Contact';
 import './styles.css';
 
+const usePageViews = () => {
+  let location = useLocation();
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('config', 'G-ZKJE5EH3BJ', {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+};
+
 const App = () => {
+  usePageViews();
+
   return (
-    <div className="App">
-      <Header />
-      <div className="scroll-indicator">
-        <p>SCROLL</p>
-        <div className="line"></div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="scroll-indicator">
+          <p>SCROLL</p>
+          <div className="line"></div>
+        </div>
+        <main>
+          <Switch>
+            <Route path="/timeline" component={Timeline} />
+          </Switch>
+        </main>
+        <Contact />
       </div>
-      <main>
-        <Timeline />
-      </main>
-      <Contact />
-    </div>
+    </Router>
   );
 };
 
 export default App;
+
 
 
