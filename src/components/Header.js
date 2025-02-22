4403cbbe0ea37f33d69c12d72336a8a44b3b6de7
Michael Watts <wattsjmichael@gmail.com>
2025-02-22 09:15:16 -0800

    changed location

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="header-content">
         <h1>Hello, my name is Michael.</h1>
         <p>
-          I'm a <span>VR Developer</span> and <span>Project Manager</span> based in Ridgefield, WA, with extensive experience in managing and developing VR/AR projects. I transitioned from a restaurant manager to a tech professional, utilizing my leadership skills and technical expertise to create virtual recreations for emergency preparedness. I excel in managing multiple projects, developing VR/AR solutions, and collaborating with high-profile clients.
+          I'm a <span>VR Developer</span> and <span>Project Manager</span> based in Longview, WA, with extensive experience in managing and developing VR/AR projects. I transitioned from a restaurant manager to a tech professional, utilizing my leadership skills and technical expertise to create virtual recreations for emergency preparedness. I excel in managing multiple projects, developing VR/AR solutions, and collaborating with high-profile clients.
         </p>
         <a href="https://www.oregonlive.com/business/2021/12/after-layoff-restaurant-manager-finds-new-career-in-virtual-reality-passion.html" target="_blank" rel="noopener noreferrer">
           Read my story on OregonLive
